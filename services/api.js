adc622c0fe83500d59ca845a2b2f30db9c68d0de
Abhishek Rana <ranaabhi3010@gmail.com>
2026-08-23 03:03:53 +0530

    Update mobile app API URL

--- services/api.js
+++ services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const API = axios.create({
-  baseURL: "https://backend-behavior-pred-depoy.onrender.com/api",
+  baseURL: "https://api.bhuvans.in/api",
   timeout: 10000,
 });
 
